18451576c2049fc2637487a683756b0f44823b32
ahmatguerdi <ahmatguerdi9@gmail.com>
2025-06-27 16:01:24 +0100

    deuxième commit

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MyProfil } from './my-profil/my-profil';
+import { Onboarding } from './onboarding/onboarding';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [
+    MyProfil, Onboarding, 
+  ],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
---
+++ src/app/my-profil/my-profil.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-my-profil',
+  imports: [],
+  templateUrl: './my-profil.html',
+  styleUrl: './my-profil.css'
+})
+export class MyProfil {
+
+}
